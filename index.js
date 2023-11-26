eedfc6f0002edf4d47af7ad6600bbe5297508f4a
alfurahman <96514109+malpu11@users.noreply.github.com>
2023-11-26 07:45:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const updateFoodPosition = () => {
 
 const handleGameOver = () => {
     clearInterval(setIntervalId);
-    alert("Game Over! Press OK to replay...");
+    alert("yah kalahh hahaha ulang lagi");
     location.reload();
 }
 
